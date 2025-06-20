cb4171010699ab5b09a6582191880d355b37a205
hoslim <ghtjd5087@gmail.com>
2025-06-20 17:17:31 +0900

    feat: cors set up

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 from . import db
 from .routes import main as main_blueprint
@@ -12,6 +13,14 @@ from .routes.users import bp as users_bp
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
+    CORS(app, resources={
+        r"/api/*": {
+            "origins": ["http://localhost:3000", "http://127.0.0.1:3000", "https://port-0-likebike-mc4iz1js1403f457.sel5.cloudtype.app"],
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization", "X-Admin"]
+        }
+    })
 
     # PostgreSQL 연결 설정
     app.config.from_mapping(
